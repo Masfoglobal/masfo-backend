93db2eb5c0e43d3de82489783c974e98c883b127
MASFO GLOBAL CONNECT LTD <infomasfo@gmail.com>
2026-02-15 05:04:36 +0000

    fix config for sqlite

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = "masfo_ultra_secure_key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
