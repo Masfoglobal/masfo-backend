a24e7bcf6f6da85c6c5f8774c5ce2522fa660240
MASFO GLOBAL CONNECT LTD <infomasfo@gmail.com>
2026-02-18 16:11:57 +0000

    addwd user_id to post

--- models.py
+++ models.py
@@ -29,4 +29,6 @@ class Post(db.Model):
     title = db.Column(db.String(200), nullable=False)
     body = db.Column(db.Text, nullable=False)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
